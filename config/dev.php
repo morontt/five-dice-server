faddbfeac992e07c4ba23edf7f1b3bafb518cb2f
morontt <morontt@gmail.com>
2015-10-09 00:19:48 +0300

    base setup Silex

---
+++ config/dev.php
@@ -0,0 +1,14 @@
+<?php
+
+use Silex\Provider\WebProfilerServiceProvider;
+
+// include the prod configuration
+require __DIR__.'/prod.php';
+
+// enable the debug mode
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\HttpFragmentServiceProvider());
+$app->register(new WebProfilerServiceProvider(), array(
+    'profiler.cache_dir' => __DIR__.'/../var/cache/profiler',
+));
