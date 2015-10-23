b5aa6ee53958f59475bcf9936cb847dbedcbebd7
morontt <morontt@gmail.com>
2015-10-23 22:31:01 +0300

    add transitions #6

--- src/Finite/StateMachine.php
+++ src/Finite/StateMachine.php
@@ -14,6 +14,7 @@ class StateMachine
     const STATE_ROLLING_1 = 'rolling_1';
     const STATE_ROLLING_2 = 'rolling_2';
     const STATE_ROLLING_3 = 'rolling_3';
+    const STATE_SCORE = 'score';
     const STATE_CLOSED = 'closed';
 
     /**
@@ -32,6 +33,16 @@ class StateMachine
         $this->sm->addState(self::STATE_ROLLING_2);
         $this->sm->addState(self::STATE_ROLLING_3);
         $this->sm->addState(new State(self::STATE_CLOSED, StateInterface::TYPE_FINAL));
+
+        $this->sm->addTransition('start', self::STATE_PENDING, self::STATE_REPLACEMENT_PLAYER);
+        $this->sm->addTransition('end', self::STATE_REPLACEMENT_PLAYER, self::STATE_CLOSED);
+        $this->sm->addTransition('roll_1', self::STATE_REPLACEMENT_PLAYER, self::STATE_ROLLING_1);
+        $this->sm->addTransition('roll_2', self::STATE_REPLACEMENT_PLAYER, self::STATE_ROLLING_2);
+        $this->sm->addTransition('roll_3', self::STATE_REPLACEMENT_PLAYER, self::STATE_ROLLING_3);
+        $this->sm->addTransition('next', self::STATE_SCORE, self::STATE_REPLACEMENT_PLAYER);
+        $this->sm->addTransition('score_1', self::STATE_ROLLING_1, self::STATE_SCORE);
+        $this->sm->addTransition('score_2', self::STATE_ROLLING_2, self::STATE_SCORE);
+        $this->sm->addTransition('score_3', self::STATE_ROLLING_3, self::STATE_SCORE);
     }
 
     /**
